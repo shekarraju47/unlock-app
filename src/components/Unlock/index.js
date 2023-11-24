5311355fe72da5c7ef49dddbc502872e8c8a4768
shekar <shekarraju47@outlook.com>
2023-11-24 06:51:22 +0000

    first commint

---
+++ src/components/Unlock/index.js
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+
+import {MainContainer, LockImg, Button, Heading} from './styledComponents'
+
+const Unlock = () => {
+  const [isLocked, setLocked] = useState(true)
+
+  const lockedImg = isLocked
+    ? 'https://assets.ccbp.in/frontend/hooks/lock-img.png'
+    : 'https://assets.ccbp.in/frontend/hooks/unlock-img.png'
+
+  const Handler = () => {
+    setLocked(prev => !prev)
+  }
+  return (
+    <>
+      <MainContainer>
+        <LockImg alt={isLocked ? 'lock' : 'unlock'} src={lockedImg} />
+        <Heading>
+          {isLocked ? 'Your Device is Locked' : 'Your Device is Unlocked'}
+        </Heading>
+        <Button onClick={Handler}>{isLocked ? 'Unlock' : 'Lock'}</Button>
+      </MainContainer>
+    </>
+  )
+}
+
+export default Unlock
---
+++ src/components/Unlock/styledComponents.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: linear-gradient(to top, #3c2940, #0b0c1e);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Heading = styled.p`
+  color: #ffffff;
+  font-family: 'Roboto';
+  font-size: 28px;
+`
+
+export const LockImg = styled.img`
+  width: 100px;
+  height: 100px;
+`
+
+export const Button = styled.button`
+  border: none;
+  color: #ffffff;
+  background-color: #06b6d4;
+  padding: 8px;
+  border-radius: 6px;
+`
